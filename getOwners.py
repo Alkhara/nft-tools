345a7fd0d67ca1f86cd07dfb2975b8e8dd767ca9
Alkhara <cawley22@gmail.com>
2022-03-17 03:27:07 -0400

    fix

--- getOwners.py
+++ getOwners.py
@@ -1,15 +1,12 @@
 from web3 import Web3
 from web3.exceptions import ContractLogicError
-from web3.middleware import geth_poa_middleware
-from hexbytes import HexBytes as hb
 from dotenv import load_dotenv
+import argparse
 import datetime
 import requests
 import json
-import time
 import sys
-import csv
-import os
+
 
 # Load up your keys
 load_dotenv()
@@ -18,18 +15,23 @@ etherscan_key = str(os.getenv('ETHERSCAN_KEY'))
 
 # Mainnet
 w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/' + infura_key))
-collection = sys.argv[1]
+
+# Accept script arguments
+parser = argparse.ArgumentParser(description='Provide the NFT Collection Name')
+parser.add_argument('--collection', help='--collection BAYC')
+
+s = vars(parser.parse_args())
 
 # Lookup the Collection Info
-if collection:
+if 'collection' in s.keys():
 	try:
 		f = open('collections.json')
 		info = json.load(f)
-		nft = info[collection]
+		nft = info[s['collection']]
 	except:
 		print("Collection not found, add it!")
 else:
-	print('Provide the collection argument!')
+	print('Provide the collection argument! ( --collection BAYC)')
 	exit()
 
 
@@ -43,11 +45,13 @@ def abiLookupByAddress(addr):
 
 # Check NFT Token Index
 def checkIndex():
-		# Get the NFT Contract ABI
+	# Get the NFT Contract ABI
 	nftABI = abiLookupByAddress(w3.toChecksumAddress(nft))
-		#Load the Contract Instance
+
+	#Load the Contract Instance
 	nftContract = w3.eth.contract(address=nft, abi=nftABI)
 
+	# Check if Token 0 returns, else start at 1
 	try:
 		owner = nftContract.functions.ownerOf(0).call()
 		print('Token Index starts at 0')
@@ -62,18 +66,24 @@ def checkIndex():
 def getOwners(nftIndexStart):
 	# Get the NFT Contract ABI
 	nftABI = abiLookupByAddress(w3.toChecksumAddress(nft))
+
 	# Load the Contract Instance
 	nftContract = w3.eth.contract(address=nft, abi=nftABI)
+
 	# Get the Total Supply of NFTs
 	nftCount = nftContract.functions.totalSupply().call()
 
 	# Set the NFT Token Index
 	x = nftIndexStart
+
 	# Initialize the owners Index
 	y = 0
+
 	# Initialize the owners List
 	owners = []
 	token = []
+
+	# Collect the Token Owners
 	while x <= nftCount:
 		try:
 			owner = nftContract.functions.ownerOf(x).call()
@@ -98,8 +108,8 @@ def write(owners):
 
 
 def main():
-	getOwners(checkIndex())
-	write(owners)
+	_owners = getOwners(checkIndex())
+	write(_owners)
 
 
 main()
